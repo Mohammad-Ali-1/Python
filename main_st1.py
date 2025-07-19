be305dd0b1907638ab9ed2fae7dc24496cd41123
Muhammad Ali <146005202+Mohammad-Ali-1@users.noreply.github.com>
2025-07-19 04:04:16 -0700

    Create main_st1.py
    String introdcution

---
+++ main_st1.py
@@ -0,0 +1,14 @@
+name="Mehmet"
+friend="Hasan"
+anotherfriend="Hussain"
+print("Hello" , name)
+apple= '''Hi Am ali
+ I am from Pakistan
+ I am 20 years old'''
+# that a string with triple colons
+print(apple)
+print(apple[1])
+print(apple[0:5])
+# looping through string
+for character in apple:
+  print(character)
