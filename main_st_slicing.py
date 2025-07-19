29e1c22177254894cf0d87ea0b7ece20f6dc91b7
Muhammad Ali <146005202+Mohammad-Ali-1@users.noreply.github.com>
2025-07-19 04:36:07 -0700

    Create main_st_slicing.py
    Slicing of Strings

---
+++ main_st_slicing.py
@@ -0,0 +1,16 @@
+name="Mehmet,Ibrahim"
+print(len(name))
+print(name[0:6])
+
+fruitname="banana"
+length=len(fruitname)
+print("Banana is a ",length,"letter word")
+# slicing of strings
+print(name[0:5])
+print(name[1:5])
+print(name[:-5])
+print(name[:-1])
+print(name[-9:-4])
+# it working in reverse order
+name1=" Mehmet,Ibrahim"
+print(name1[-4:-2])
