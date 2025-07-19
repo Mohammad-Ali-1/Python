2f576f0768b2ff559001ccf4782f8113d55d56ae
Muhammad  Ali <45204725-mrali8639@users.noreply.replit.com>
2025-07-19 08:08:37 +0000

    Python Variable & Data types

---
+++ main.py
@@ -0,0 +1,18 @@
+a = complex(8,2)
+print(a)
+b = "harry"
+c=True
+a1=9
+print(a+a1)
+print("The type of a is", type(a))
+print("The type of a is", type(b))
+print("The type of a is",type(c))
+
+list1=[8,2.3,[-4,5],["apple","banana"]]
+print((list1))
+
+tuple1=(("parrot","sparrrow"),("lion","tiger"))  
+print(tuple1)
+
+dict1={"name":"sakshi","age":20,"canVote":True}
+print(dict1)
